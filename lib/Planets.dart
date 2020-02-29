7fa23539b49d6fc630970645bc9faa654d7b76b0
thiagohbfialho <thiago.fialhoti@gmail.com>
2020-02-29 01:23:34 -0300

    atualizacao do planets

--- lib/Planets.dart
+++ lib/Planets.dart
@@ -2,10 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class PlanetsGrid extends StatelessWidget {
+  List<String> dataSource = [];
+  List<String> namePlanet = ['Vegeta','Majin','Namek','Earth'];
+
+  PlanetsGrid() {
+    List<String> namePlanet = ['Vegeta','Gohan','Goku','Piccolo','Cell','Goten','Krillin'];
+    for (var i = 0; i < 7; i++) {
+      var nome = namePlanet.elementAt(i);
+      dataSource.add("https://dragon-ball-api.herokuapp.com/images/$nome.jpg");
+    }
+  } 
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text(
+          "PLANETS",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+      child: Container(
+        color: Colors.blueGrey.withOpacity(0.8),
+        child: GridView.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          padding: EdgeInsets.all(8),
+          children: dataSource.map((value) {
+            return Image.network(value);
+          }).toList(),
+        ),
+      ),
     );
   }
 }
