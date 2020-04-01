dc3bee52e9d306ad14a8054c8cab5b1d1808a958
eperegrino <epps@cesar.school>
2020-04-01 11:19:11 -0300

    commit onboarding.dart em mvvm

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -2,30 +2,80 @@ import 'package:dragonball_project/tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class OnboardingModel {
+  String onboardingImage;
+  bool isVisible;
+  String onboardingText;
+}
+
+class OnboardingViewModel {
+  OnboardingModel onboardingModel;
+
+  OnboardingViewModel() {
+    onboardingModel = OnboardingModel();
+  }
+
+  void setImage(String image) {
+    onboardingModel.onboardingImage = image;
+  }
+
+  String getImage() {
+    return onboardingModel.onboardingImage;
+  }
+
+  void setVisible(bool visible) {
+    onboardingModel.isVisible = visible;
+  }
+
+  bool isVisible() {
+    return onboardingModel.isVisible;
+  }
+
+  void setText(String text) {
+    onboardingModel.onboardingText = text;
+  }
+
+  String getText() {
+    return onboardingModel.onboardingText;
+  }
+}
+
 class OnboardingPagination extends StatelessWidget {
+  OnboardingViewModel onboardingViewModel1;
+  OnboardingViewModel onboardingViewModel2;
+
+  OnboardingPagination() {
+    onboardingViewModel1 = OnboardingViewModel();
+    onboardingViewModel1.setImage("images/dbz1.png");
+    onboardingViewModel1.setVisible(false);
+    onboardingViewModel1.setText("");
+    onboardingViewModel2 = OnboardingViewModel();
+    onboardingViewModel2.setImage("images/dbz.png");
+    onboardingViewModel2.setVisible(true);
+    onboardingViewModel2.setText("Oi, eu sou o Goku!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageView(children: <Widget>[
-        Onboarding("images/dbz1.png", false, ""),
-        Onboarding("images/dbz.png", true, "Oi, eu sou o Goku!"),
+        Onboarding(onboardingViewModel1),
+        Onboarding(onboardingViewModel2),
       ],
     );
   }
 }
 
 class Onboarding extends StatelessWidget {
-  final String onboardingImage;
-  final bool isVisible;
-  final String onboardingText;
+  OnboardingViewModel onboardingViewModel;
 
-  Onboarding(this.onboardingImage, this.isVisible, this.onboardingText);
+  Onboarding(this.onboardingViewModel);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(
         children: <Widget>[
-          OnboardingBackground(onboardingImage),
+          OnboardingBackground(onboardingViewModel.getImage()),
           Container(
             color: Colors.black.withOpacity(0.5),
           ),
@@ -39,14 +89,14 @@ class Onboarding extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: const EdgeInsets.only(bottom: 20),
-                      child: Text(onboardingText, style: TextStyle(color: Colors.white, fontSize: 30),),
+                      child: Text(onboardingViewModel.getText(), style: TextStyle(color: Colors.white, fontSize: 30),),
                     ),
                     // Padding(
                     //   padding: const EdgeInsets.only(bottom: 70),
                     //   child: Text("Descubra informações dos personagens do mundo de Dragon Ball", style: TextStyle(color: Colors.white, fontSize: 15),),
                     // ),
                     Visibility(
-                      visible: isVisible,
+                      visible: onboardingViewModel.isVisible(),
                       child: CupertinoButton(
                         child: Text("Vamos começar!"),
                         color: Colors.deepOrange,
